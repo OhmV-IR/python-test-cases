e4c82b8b614e896318014039cd4da92416b20258
Adrien B <bourdeaux.adrien@gmail.com>
2024-03-26 22:36:22 -0400

    works at a basic level with some bugs

--- python-test-cases/Program.cs
+++ python-test-cases/Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+using System.Diagnostics;
+using System;
+using System.Text;
+using System.Threading;
+
+int timeout;
+try
+{
+    timeout = int.Parse(args[0]) * 1000;
+}
+catch
+{
+    timeout = 60 * 1000;
+}
+var currentFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+string[] testsFile = new string[1];
+try
+{
+    testsFile = File.ReadAllLines(Path.Combine(currentFolder, "tests.txt"));
+}
+catch
+{
+    Console.WriteLine("Tests file not found or contained invalid data, exiting.");
+    Environment.Exit(1);
+}
+string fileToRun = testsFile[0];
+// Delete any existing text
+File.WriteAllText(Path.Combine(currentFolder, "results.txt"), "");
+using (Process process = new Process())
+{
+    process.StartInfo.FileName = "python";
+    process.StartInfo.Arguments = fileToRun;
+    process.StartInfo.UseShellExecute = false;
+    process.StartInfo.RedirectStandardOutput = true;
+    process.StartInfo.RedirectStandardInput = true;
+
+    using (AutoResetEvent outputWaitHandle = new AutoResetEvent(false))
+    {
+        process.OutputDataReceived += (sender, e) => {
+            if (e.Data == null)
+            {
+                outputWaitHandle.Set();
+            }
+            else
+            {
+                File.AppendAllText(Path.Combine(currentFolder, "results.txt"), e.Data + "\n");
+            }
+        };
+
+        process.Start();
+        var counter = 0;
+        while (true)
+        {
+            if (testsFile[counter] == "endofcase")
+            {
+                break;
+            }
+            else
+            {
+                process.StandardInput.WriteLine(testsFile[counter]);
+                counter = counter + 1;
+            }
+        }
+        process.BeginOutputReadLine();
+        if (process.WaitForExit(timeout) &&
+        outputWaitHandle.WaitOne(timeout))
+        {
+            // Process completed. Check process.ExitCode here.
+        }
+        else
+        {
+            Console.WriteLine("Test timed out");
+        }
+    }
+}
